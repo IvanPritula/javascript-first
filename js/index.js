c8e9131bbde6b3cfe7b39ac5793029fe1e646701
Ivan Pritula <ivanpritula2000@gmail.com>
2022-01-25 16:20:46 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+let num = 0;
+num +=10;
+
+num *= 3;
+num = '123';
+console.log(num);
+
+console.log(num === 20);
+
+console.log(num <= 20);
+
+console.log(num && 20 && '1000b');
+
+console.log( typeof num);
+
+const typeofNum = typeof num;
+console.log(typeof typeofNum);
+
+const userInput = prompt('Insert number');
+const userNumber = Number(userInput);
+console.log(userInput === 'number');
